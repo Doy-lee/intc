199ef27fe374015e3b7e315fecbe22b3ba99b1e1
doyle <doylet@protonmail.com>
2021-09-12 18:56:06 +1000

    gcc: Explicitly set gcc to compile in c99

--- build_tests.sh
+++ build_tests.sh
@@ -6,10 +6,8 @@ mkdir --parents build
 echo "[SCRIPT] Building tests via gcc to build/intc_tests_cpp_gcc"
 pushd build
 g++ -O2 -D INTC_TESTS_WITH_MAIN -I ${script_dir} ${script_dir}/intc_tests.c -o intc_tests_cpp_gcc
-popd
 
 echo "[SCRIPT] Building tests via gcc to build/intc_tests_c_gcc"
-pushd build
-gcc -O2 -D INTC_TESTS_WITH_MAIN -I ${script_dir} ${script_dir}/intc_tests.c -o intc_tests_c_gcc
+gcc -std=c99 -O2 -D INTC_TESTS_WITH_MAIN -I ${script_dir} ${script_dir}/intc_tests.c -o intc_tests_c_gcc
 popd
 
